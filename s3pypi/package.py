8143ef379ad40efb1ff25fca4a28fbeafd48e7f6
William McBroom <william.mcbroom@draft2digital.com>
2017-02-21 16:59:32 -0600

    Add decode method call to text variable to prevent errors of "TypeError: cannot use a string pattern on a bytes-like object"

--- s3pypi/package.py
+++ s3pypi/package.py
@@ -38,7 +38,7 @@ class Package(object):
 
     @staticmethod
     def _find_package_name(text):
-        match = re.search('^(copying files to|making hard links in) (.+)\.\.\.', text, flags=re.MULTILINE)
+        match = re.search('^(copying files to|making hard links in) (.+)\.\.\.', text.decode('utf-8'), flags=re.MULTILINE)
 
         if not match:
             raise RuntimeError('Package name not found in:\n' + text)
