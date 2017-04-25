7592dd6289f5bef5a7104a7af7e8b94f5f6622f3
Andrew Scott <andrew.mark.scott@gmail.com>
2017-04-25 23:17:40 +1000

    Refactored use of argparse to allow testing

--- s3pypi/cli.py
+++ s3pypi/cli.py
@@ -24,14 +24,18 @@ def create_and_upload_package(args):
     storage.put_index(package, index)
 
 
-def main():
+def parse_args(raw_args):
     p = argparse.ArgumentParser(prog=__prog__)
     p.add_argument('--bucket', required=True, help='S3 bucket')
     p.add_argument('--secret', help='S3 secret')
     p.add_argument('--region', help='S3 region')
     p.add_argument('--force', action='store_true', help='Overwrite existing packages')
     p.add_argument('--no-wheel', dest='wheel', action='store_false', help='Skip wheel distribution')
-    args = p.parse_args()
+    return p.parse_args(raw_args)
+
+
+def main():
+    args = parse_args(sys.argv[1:])
 
     try:
         create_and_upload_package(args)
