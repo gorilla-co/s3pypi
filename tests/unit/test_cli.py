edfa66250680b0c1415b759e267fce56655bfe75
Andrew Scott <andrew.mark.scott@gmail.com>
2017-04-25 23:19:07 +1000

    New test for invalid argparse kwargs

---
+++ tests/unit/test_cli.py
@@ -0,0 +1,10 @@
+import pytest
+
+from s3pypi.cli import parse_args
+
+
+def test_cli_argparser_raises_no_exceptions():
+    """An invalid keyword to ArgumentParser was causing an exception in Python 3."""
+    with pytest.raises(SystemExit):
+        args = parse_args(None)
+
