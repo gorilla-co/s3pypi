7403148fbfe835d75d280b41a68cde3f38157119
Richard Owen <richardowen@users.noreply.github.com>
2017-09-12 18:05:17 +0100

    Add support for S3 Server-Side Encryption

--- s3pypi/storage.py
+++ s3pypi/storage.py
@@ -13,12 +13,13 @@ __license__ = 'MIT'
 class S3Storage(object):
     """Abstraction for storing package archives and index files in an S3 bucket."""
 
-    def __init__(self, bucket, secret=None, region=None, bare=False, private=False):
+    def __init__(self, bucket, secret=None, region=None, bare=False, private=False, sse=False):
         self.s3 = boto3.resource('s3', region_name=region)
         self.bucket = bucket
         self.secret = secret
         self.index = '' if bare else 'index.html'
         self.acl = 'private' if private else 'public-read'
+        self.sse = sse
 
     def _object(self, package, filename):
         path = '%s/%s' % (package.directory, filename)
@@ -32,18 +33,28 @@ class S3Storage(object):
             return Index([])
 
     def put_index(self, package, index):
-        self._object(package, self.index).put(
-            Body=index.to_html(),
-            ContentType='text/html',
-            CacheControl='public, must-revalidate, proxy-revalidate, max-age=0',
-            ACL=self.acl
-        )
+        params = {
+            'Body': index.to_html(),
+            'ContentType': 'text/html',
+            'CacheControl': 'public, must-revalidate, proxy-revalidate, max-age=0',
+            'ACL': self.acl,
+        }
+
+        if self.sse:
+            params['ServerSideEncryption'] = 'AES256'
+
+        self._object(package, self.index).put(**params)
 
     def put_package(self, package):
         for filename in package.files:
             with open(os.path.join('dist', filename), mode='rb') as f:
-                self._object(package, filename).put(
-                    Body=f,
-                    ContentType='application/x-gzip',
-                    ACL=self.acl
-                )
+                params = {
+                    'Body': f,
+                    'ContentType': 'application/x-gzip',
+                    'ACL': self.acl,
+                }
+
+                if self.sse:
+                    params['ServerSideEncryption'] = 'AES256'
+
+                self._object(package, filename).put(**params)
