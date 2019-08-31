ccb57f544512a3182023cece3757ed557c21347e
Christoph Ludwig <christoph.ludwig@haufe-lexware.com>
2019-08-31 21:06:27 +0200

    add admin and infrastructure scripts to setup

--- setup.py
+++ setup.py
@@ -1,22 +1,28 @@
 from setuptools import setup, find_packages
 
-from s3pypi import __prog__, __version__
-
+import s3pypi.admin
+import s3pypi.infrastructure
 
 setup(
-    name=__prog__,
-    version=__version__,
+    name='s3pypi-auth',
+    version=s3pypi.__version__,
 
     description='CLI tool for creating a Python Package Repository in an S3 bucket',
 
     author='Ruben Van den Bossche, Matteo De Wint',
     author_email='ruben@novemberfive.co, matteo@novemberfive.co',
     url='https://github.com/novemberfiveco/s3pypi',
-    download_url='https://github.com/novemberfiveco/s3pypi/tarball/' + __version__,
+    download_url='https://github.com/novemberfiveco/s3pypi/tarball/' + s3pypi.__version__,
 
-    packages=find_packages(),
-    package_data={__prog__: ['templates/*.j2']},
+    packages=find_packages(exclude=('s3pypi_auth', )),
+    package_data={s3pypi.__prog__: ['templates/*.j2']},
 
-    install_requires=['boto3', 'Jinja2>=2.7', 'wheel'],
-    entry_points={'console_scripts': ['{0}={0}.__main__:main'.format(__prog__)]},
+    install_requires=['basicauth', 'boto3', 'Jinja2>=2.7', 'passlib>=1.7', 'wheel'],
+    entry_points={
+        'console_scripts': [
+            '{0}={0}.__main__:main'.format(s3pypi.__prog__),
+            '{0}={1}.__main__:main'.format(s3pypi.admin.__prog__, s3pypi.admin.__package__),
+            '{0}={1}.__main__:main'.format(s3pypi.infrastructure.__prog__, s3pypi.infrastructure.__package__)
+        ]
+    },
 )
