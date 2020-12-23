11cd3c77a87825edc119060bf4ef0f07cc1d7c67
Matteo De Wint <matteo@gorilla.co>
2020-12-23 14:00:35 +0100

    Improve integration test

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -8,7 +8,12 @@ def test_main_upload_package(chdir, data_dir, s3_bucket):
         dist = sorted(glob.glob("dists/*"))
         s3pypi(*dist, "--bucket", s3_bucket.name)
 
-    assert s3_bucket.Object("foo/").get()
-    assert s3_bucket.Object("foo/foo-0.1.0.tar.gz").get()
-    assert s3_bucket.Object("hello-world/").get()
-    assert s3_bucket.Object("hello-world/hello_world-0.1.0-py3-none-any.whl").get()
+    def read(key: str) -> bytes:
+        return s3_bucket.Object(key).get()["Body"].read()
+
+    def assert_pkg_exists(prefix: str, filename: str):
+        assert read(prefix + filename)
+        assert f">{filename}</a>" in read(prefix).decode()
+
+    assert_pkg_exists("foo/", "foo-0.1.0.tar.gz")
+    assert_pkg_exists("hello-world/", "hello_world-0.1.0-py3-none-any.whl")
