e324b580ccf93875f12ab51ce372027795a4fc20
Matteo De Wint <matteo@gorilla.co>
2020-12-24 01:00:37 +0100

    Add integration test for uploading existing package

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -1,7 +1,9 @@
 import glob
+import logging
 
 import pytest
 
+from s3pypi import __prog__
 from s3pypi.__main__ import main as s3pypi, string_dict
 
 
@@ -32,3 +34,18 @@ def test_main_upload_package(chdir, data_dir, s3_bucket):
 
     assert_pkg_exists("foo/", "foo-0.1.0.tar.gz")
     assert_pkg_exists("hello-world/", "hello_world-0.1.0-py3-none-any.whl")
+
+
+def test_main_upload_package_exists(chdir, data_dir, s3_bucket, caplog):
+    dist = "dists/foo-0.1.0.tar.gz"
+
+    with chdir(data_dir):
+        for _ in range(2):
+            s3pypi(dist, "--bucket", s3_bucket.name)
+        s3pypi(dist, "--force", "--bucket", s3_bucket.name)
+
+    msg = "foo-0.1.0.tar.gz already exists! (use --force to overwrite)"
+    success = (__prog__, logging.INFO, "Uploading " + dist)
+    warning = (__prog__, logging.WARNING, msg)
+
+    assert caplog.record_tuples == [success, warning, success]
