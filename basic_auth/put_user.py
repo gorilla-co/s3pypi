a7d6e4b987a4f1a81fec5099e310ee0eb19c1818
Matteo De Wint <matteo@gorilla.co>
2021-01-04 16:50:45 +0100

    Add more options to basic_auth/put_user.py

--- basic_auth/put_user.py
+++ basic_auth/put_user.py
@@ -3,6 +3,7 @@ import argparse
 import getpass
 import json
 import secrets
+import sys
 from dataclasses import asdict
 
 import boto3
@@ -12,17 +13,40 @@ from handler import User, hash_password, region
 
 def get_arg_parser():
     p = argparse.ArgumentParser()
-    p.add_argument("domain")
-    p.add_argument("username")
-    p.add_argument("--salt-nbytes", type=int, default=32)
-    p.add_argument("--overwrite", action="store_true")
+    p.add_argument("domain", help="S3PyPI domain")
+    p.add_argument("username", help="Username")
+    pw = p.add_mutually_exclusive_group()
+    pw.add_argument(
+        "--password-stdin", action="store_true", help="Read password from stdin"
+    )
+    pw.add_argument(
+        "--random-password",
+        metavar="N",
+        type=int,
+        default=0,
+        help="Generate a random password of N bytes",
+    )
+    p.add_argument(
+        "--salt-nbytes",
+        metavar="N",
+        type=int,
+        default=32,
+        help="Length of the random salt in bytes",
+    )
+    p.add_argument("--overwrite", action="store_true", help="Overwrite existing users")
     return p
 
 
 def main():
     args = get_arg_parser().parse_args()
 
-    password = getpass.getpass()
+    if args.password_stdin:
+        password = sys.stdin.read().strip("\n")
+    elif args.random_password:
+        password = secrets.token_urlsafe(args.random_password)
+    else:
+        password = getpass.getpass()
+
     salt = secrets.token_urlsafe(args.salt_nbytes)
 
     user = User(
@@ -31,6 +55,9 @@ def main():
         password_salt=salt,
     )
     put_user(args.domain, user, args.overwrite)
+
+    if args.random_password:
+        print(password)
 
 
 def put_user(domain: str, user: User, overwrite: bool = False):
