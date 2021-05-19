bb27c063302d9903a22f222b59154d3c74325f28
Matteo De Wint <matteo@gorilla.co>
2021-05-20 01:29:08 +0200

    Locking; hash LockID + improved timeout error message

--- s3pypi/locking.py
+++ s3pypi/locking.py
@@ -1,20 +1,17 @@
 import abc
 import datetime as dt
-import getpass
+import hashlib
+import json
 import time
 from contextlib import contextmanager
 
 import boto3
 
 
-class LockTimeoutError(Exception):
-    def __init__(self, lock_id: str):
-        super().__init__(f"Timed out trying to acquire lock '{lock_id}'")
-
-
 class Locker(abc.ABC):
     @contextmanager
-    def __call__(self, lock_id: str):
+    def __call__(self, key: str):
+        lock_id = hashlib.sha1(key.encode()).hexdigest()
         self._lock(lock_id)
         try:
             yield
@@ -43,14 +40,14 @@ class DynamoDBLocker(Locker):
         session: boto3.session.Session,
         table: str,
         poll_interval: int = 1,
-        max_attempts: int = 30,
+        max_attempts: int = 10,
     ):
         db = session.resource("dynamodb")
         self.table = db.Table(table)
         self.exc = self.table.meta.client.exceptions
         self.poll_interval = poll_interval
         self.max_attempts = max_attempts
-        self.user = getpass.getuser()
+        self.caller_id = session.client("sts").get_caller_identity()["Arn"]
 
     def _lock(self, lock_id: str):
         for attempt in range(1, self.max_attempts + 1):
@@ -60,7 +57,7 @@ class DynamoDBLocker(Locker):
                     Item={
                         "LockID": lock_id,
                         "AcquiredAt": now.isoformat(),
-                        "Username": self.user,
+                        "Owner": self.caller_id,
                     },
                     ConditionExpression="attribute_not_exists(LockID)",
                 )
@@ -69,7 +66,19 @@ class DynamoDBLocker(Locker):
                 if attempt < self.max_attempts:
                     time.sleep(self.poll_interval)
 
-        raise LockTimeoutError(lock_id)
+        item = self.table.get_item(Key={"LockID": lock_id})["Item"]
+        raise DynamoDBLockTimeoutError(self.table.name, item)
 
     def _unlock(self, lock_id: str):
         self.table.delete_item(Key={"LockID": lock_id})
+
+
+class DynamoDBLockTimeoutError(Exception):
+    def __init__(self, table: str, item: dict):
+        key = json.dumps({"LockID": {"S": item["LockID"]}})
+        super().__init__(
+            f"Timed out trying to acquire lock:\n\n{json.dumps(item, indent=2)}\n\n"
+            "Another instance of s3pypi may currently be holding the lock.\n"
+            "If this is not the case, you may release the lock as follows:\n\n"
+            f"$ aws dynamodb delete-item --table-name {table} --key '{key}'\n"
+        )
