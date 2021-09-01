425bc151f1e39d61dc2d072f4a40273c2c557994
Matteo De Wint <matteo@gorilla.co>
2021-09-01 17:12:17 +0200

    Terraform; TLSv1.2_2021

--- terraform/modules/s3pypi/main.tf
+++ terraform/modules/s3pypi/main.tf
@@ -49,7 +49,7 @@ resource "aws_cloudfront_distribution" "cdn" {
 
   viewer_certificate {
     acm_certificate_arn      = data.aws_acm_certificate.viewer.arn
-    minimum_protocol_version = "TLSv1.1_2016"
+    minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
   }
 
